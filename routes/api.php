8bb01e95534f1d8f071a3d47a00c204966297158
Wesley Lima <wesleyjoga500@gmail.com>
2019-07-29 00:19:10 -0300

    Aula Validation

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('mostraAula/{id}','AulaController@showAula')->name('mostra');
+Route::get('mostraAula/{id}','AulaController@showAula')->name('mostraAula');
 Route::get('listaAula','AulaController@listAula');
 Route::post('criaAula','AulaController@createAula');
 Route::put('atualizaAula/{id}','AulaController@updateAula');
 Route::delete('deletaAula/{id]','AulaController@deleteAula');
+
+Route::get('mostraAluno/{id}','AlunoController@showAluno')->name('mostraAluno');
+Route::get('listaAluno','AlunoController@listAluno');
+Route::post('criaAluno','AlunoController@createAluno');
+Route::put('atualizaAluno/{id}','AlunoController@updateAluno');
+Route::delete('deletaAluno/{id]','AlunoController@deleteAluno');
